0f231ac4a0b0b79796ee88f01671df7cfb4fd4c0
Yuriy Golota <ypgolota@pm.me>
2022-06-01 11:17:38 -0400

    added more divs, WIP

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const customerList = document.getElementById('customers');
 for (let c of customers) {
-    // Each entry box
+    
+    // Creating a box for each entry
     let customerElement = document.createElement('div');
     customerElement.classList.add('entry');
 
@@ -10,11 +11,33 @@ for (let c of customers) {
     customerImg.src = c.picture.large;
     customerElement.appendChild(customerImg);
 
-    // 
+    // Name
+    let customerName = document.createElement('div');
+    customerName.classList.add('name');
+    customerName.innerText = `${c.name.first.charAt(0).toUpperCase() + c.name.first.slice(1)} ${c.name.last.charAt(0).toUpperCase() + c.name.first.slice(1)}`;  
+    customerElement.appendChild(customerName);
+
+    // Email
+    let customerEmail = document.createElement('div'); // creating element
+    customerEmail.classList.add('email'); // add a class to the element
+    customerEmail.innerText = c.email; // filling the element with innerText
+    customerElement.appendChild(customerEmail); // putting email element into the customer element (i.e. .entry)
+
+    // Address
+    let customerAddress1 = document.createElement('div');
+    customerAddress1.classList.add('address');
+    customerAddress1.innerText = `${c.location.street.number} ${c.location.street.name}`;
+    customerElement.appendChild(customerAddress1);
+
+    let customerAddress2 = document.createElement('div');
+    customerAddress2.classList.add('address');
+    customerAddress2.innerText = `${c.location.city}, STATE ${c.location.postcode}`;
+    customerElement.appendChild(customerAddress2);
+
+    // DOB
+
+    // Registered
     
-    
-    
-    
-    
+    // Placing every entry into the "big" div
     customerList.appendChild(customerElement);
 }
