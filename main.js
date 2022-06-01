eca8844672162c7e35ef6892c2516d87b82aae00
Yuriy Golota <ypgolota@pm.me>
2022-06-01 10:07:37 -0400

    got imgs up in html

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 const customerList = document.getElementById('customers');
-for (let customer of customerList) {
+for (let c of customers) {
+    // Each entry box
+    let customerElement = document.createElement('div');
+    customerElement.classList.add('entry');
 
+    // Image
+    let customerImg = document.createElement('img');
+    customerImg.classList.add('img');
+    customerImg.src = c.picture.large;
+    customerElement.appendChild(customerImg);
+
+    // 
+    
+    
+    
+    
+    
+    customerList.appendChild(customerElement);
 }
